776cdb1099200c2120ec8ba4181bfe677cad3539
Priyansh <ishudaksh2603@gmail.com>
2022-08-25 17:32:24 +0530

    Added Firebase

--- portal/src/components/login/fire.js
+++ portal/src/components/login/fire.js
@@ -4,13 +4,15 @@
 
 import { initializeApp, getApps, getApp } from "firebase/app";
 
+import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyCgC2IQT_1dbRrxExIuyexPIo_REsjDOgI",
-    authDomain: "login-signup-9ad90.firebaseapp.com",
-    projectId: "login-signup-9ad90",
-    storageBucket: "login-signup-9ad90.appspot.com",
-    messagingSenderId: "1033388960840",
-    appId: "1:1033388960840:web:4301c5fe27cf64ae02fc7d",
+    apiKey: "AIzaSyBUV4Dbv6Z2MimSeMeddMUCuEyJXnIt5B8",
+    authDomain: "my-first-firebase-77d9a.firebaseapp.com",
+    projectId: "my-first-firebase-77d9a",
+    storageBucket: "my-first-firebase-77d9a.appspot.com",
+    messagingSenderId: "159827827078",
+    appId: "1:159827827078:web:f06f67c3cc8490476d7407",
+    measurementId: "G-YKEDMZ9614",
 };
 let fire;
 fire = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
--- portal/src/pages/Hello.jsx
+++ portal/src/pages/Hello.jsx
@@ -1,39 +1,41 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
-// import { collection, getDocs, addDoc } from 'firebase/firestore'
-// import { db } from "../components/Main/firebase-config";
+import { collection, getDocs, addDoc } from 'firebase/firestore';
+import { db } from "../components/Main/firebase-config";
 import { Link } from 'react-router-dom';
 
 const Hello = () => {
     const { value } = useParams();
     // console.log(value); 
 
-    // const useCollection = collection(db, 'users-1')
-    // const [users, setUser] = useState([])
-    // React.useEffect(() => {
-    //     const getUsers = async () => {
-    //         const data = await getDocs(useCollection);
-    //         console.log(data)
-    //         console.log(data.docs)
-    //         setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-    //     }
-    //     getUsers();
-    //     // find();
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [])
+    const useCollection = collection(db, 'users-1')
+    const [users, setUser] = useState([])
+    React.useEffect(() => {
+        const getUsers = async () => {
+            const data = await getDocs(useCollection);
+            console.log(data)
+            console.log(data.docs)
+            setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        }
+        getUsers();
+        // find();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     // const find = () => {
     //     // eslint-disable-next-line no-lone-blocks
     //     {
-    //         users.map((user) => {
-    //             if (user.Property === value) {
-    //                 return (
-    //                     <>
-    //                         <h1>Google</h1>
-    //                     </>
-    //                 )
-    //             }
-    //         })
+            users.map((user) => {
+                if (user.Property === value) {
+                    console.log(user.Name)
+                    return (
+                        <>
+                            <h1>Google {user.Property}</h1>
+                            
+                        </>
+                    )
+                }
+            })
     //     }
     // }
 
