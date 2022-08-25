f1e27ddb00cf6c98c4b75bced87dc7672748b31f
ksatyarth2 <erkumar@protonmail.ch>
2022-08-25 16:13:17 +0530

    updated firebase config

--- portal/src/components/login/fire.js
+++ portal/src/components/login/fire.js
@@ -1,6 +1,8 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp, firebase } from "firebase/app";
 // import "firebase/compat/auth";
 // import "firebase/compat/firestore";
+
+import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCgC2IQT_1dbRrxExIuyexPIo_REsjDOgI",
@@ -10,7 +12,12 @@ const firebaseConfig = {
     messagingSenderId: "1033388960840",
     appId: "1:1033388960840:web:4301c5fe27cf64ae02fc7d",
 };
-
-const fire = initializeApp(firebaseConfig);
+let fire;
+fire = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// if (!firebase.apps.length) {
+//     fire = initializeApp({ firebaseConfig });
+// } else {
+// fire = firebase.app(); // if already initialized, use that one
+// }
 
 export default fire;
